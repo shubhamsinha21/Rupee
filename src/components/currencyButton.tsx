bf8d556eb10f89b6e1c4ca930240d0c1009c5db3
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-05-20 15:52:06 +0530

    currencyButton.tsx ✅

---
+++ src/components/currencyButton.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import type { PropsWithChildren } from 'react';
+// data passing in the component will be of particularly type
+
+import {View, Text, StyleSheet} from 'react-native'
+
+type currencyButtonProps = PropsWithChildren<{
+        name:string,
+        flag:string
+}>
+
+const currencyButton = (props:currencyButtonProps):JSX.Element =>{
+  return (
+        <View style={styles.buttonContainer}>
+            <Text style={styles.flag}>{props.flag}</Text>
+            <Text style={styles.country}>{props.name}</Text>
+        </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    buttonContainer:{
+        alignItems:'center'
+    },
+    flag:{
+        fontSize:25,
+        color:'red',
+        marginBottom:4
+    },
+    country:{
+        fontSize:20,
+        color:'red',
+    },
+
+})
+export default currencyButton;
